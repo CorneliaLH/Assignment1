87ea9c7cb86cb5877460946723f3763658f09a46
Conrelia Harder <cornelialisaharder@gmail.com>
2022-03-10 15:29:47 +0100

    deploy issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   urlProducts:
     'https://medieinstitutet-wie-products.azurewebsites.net/api/products',
   urlCategories:
