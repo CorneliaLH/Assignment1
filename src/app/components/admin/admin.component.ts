fb8c08fb75041744ae31f1f64b359ed9d14b6875
Conrelia Harder <cornelialisaharder@gmail.com>
2022-03-10 16:21:48 +0100

    totalamount

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/models/Order';
+import { OrderRows } from 'src/app/models/OrderRows';
 import { Product } from 'src/app/models/Product';
 import { OrderService } from 'src/app/services/order.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -12,6 +13,8 @@ import { ProductService } from 'src/app/services/product.service';
 export class AdminComponent implements OnInit {
   myCompanyOrders: Order[] = [];
   productList: Product[] = [];
+  orderRowAmount: number = 0;
+  totalAmount: number[] = [];
   // productName: string[] = [];
 
   constructor(
@@ -40,6 +43,15 @@ export class AdminComponent implements OnInit {
     //Listan med orders hämtas från service
     this.orderService.product$.subscribe((data: Order[]) => {
       this.myCompanyOrders = data;
+      //loop för att skapa totala antalet produkter
+      this.myCompanyOrders.forEach((order) => {
+        this.orderRowAmount = 0;
+        order.orderRows.forEach((orderRow) => {
+          this.orderRowAmount += orderRow.amount;
+        });
+
+        this.totalAmount.push(this.orderRowAmount);
+      });
     });
     this.orderService.getOrders();
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  urlProducts:
+    'https://medieinstitutet-wie-products.azurewebsites.net/api/products',
+  urlCategories:
+    'https://medieinstitutet-wie-products.azurewebsites.net/api/categories',
+  urlSearch:
+    'https://medieinstitutet-wie-products.azurewebsites.net/api/search?=',
+  urlOrder:
+    'https://medieinstitutet-wie-products.azurewebsites.net/api/orders/',
 };
